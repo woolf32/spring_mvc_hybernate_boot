603d3ed4e75a0280fd5a990f7e0db1c9a77e2375
йй <woolf_32@mail.ru>
2024-06-27 19:22:49 +0300

    code style + remove am for two methods

--- src/main/java/ru/podlesnykh/springcourse/spring_mvc_hybernate_boot/services/EmployeeService.java
+++ src/main/java/ru/podlesnykh/springcourse/spring_mvc_hybernate_boot/services/EmployeeService.java
@@ -6,9 +6,13 @@ import ru.podlesnykh.springcourse.spring_mvc_hybernate_boot.entity.Employee;
 import java.util.List;
 
 public interface EmployeeService {
-    public List<Employee> showAllEmployee ();
-    public Employee getEmployeeById (int id);
+    List<Employee> showAllEmployee();
+
+    Employee getEmployeeById(int id);
+
     void save(Employee employee);
+
     void update(int id, Employee employee);
+
     void delete(int id);
 }
